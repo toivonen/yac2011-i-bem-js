eac0e705e0faba530e5500582fee87505d46da66
Varvara Stepanova <toivonen@yandex-team.ru>
2011-09-07 20:34:16 +0400

    b-cut has to react on state changing

--- blocks/b-cut/b-cut.js
+++ blocks/b-cut/b-cut.js
@@ -9,6 +9,16 @@ BEM.DOM.decl('b-cut', {
         'js' : function() {
             BEM.blocks['b-link'].liveCtxBind(this.elem('opener'), 'click', this.open, this);
             this.bindTo(this.elem('close'), 'leftclick', this.close);
+        },
+        'state' : {
+            '' : function() {
+                this.setMod(this.elem('content'), 'visibility', '');
+                this.setMod(this.elem('opener'), 'visibility', 'visible');
+            },
+            'opened' : function() {
+                this.setMod(this.elem('content'), 'visibility', 'visible');
+                this.setMod(this.elem('opener'), 'visibility', '');
+            }
         }
     },
     open : function() {
