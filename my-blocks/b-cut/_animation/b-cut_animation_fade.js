26b5f5848fa2a90ff7304f71a08b58cdca43e926
Varvara Stepanova <toivonen@yandex-team.ru>
2011-09-07 11:13:24 +0400

    jQuery makes style after animaion

--- my-blocks/b-cut/_animation/b-cut_animation_fade.js
+++ my-blocks/b-cut/_animation/b-cut_animation_fade.js
@@ -8,7 +8,11 @@ BEM.DOM.decl({ name: 'b-cut', modName: 'animation', modVal: 'fade'}, {
     onSetMod : {
         'state' : {
             'opened' : function() {
+                var _this = this;
                 this.elem('content').fadeIn();
+                this.afterCurrentEvent(function(){
+                    _this.elem('content').css('display', '');
+                });
             }
         }
     }
--- my-blocks/b-cut/_animation/b-cut_animation_smooth.js
+++ my-blocks/b-cut/_animation/b-cut_animation_smooth.js
@@ -8,7 +8,11 @@ BEM.DOM.decl({ name: 'b-cut', modName: 'animation', modVal: 'smooth'}, {
     onSetMod : {
         'state' : {
             'opened' : function() {
+                var _this = this;
                 this.elem('content').slideDown();
+                this.afterCurrentEvent(function(){
+                    _this.elem('content').css('display', '');
+                });
             }
         }
     }
